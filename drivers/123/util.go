0a8f1cc2381f5198fd1ba07fd93ec76d4249a55c
fun cat <251946192@qq.com>
2025-07-21 11:46:01 +0800

    util.go
    Signed-off-by: fun cat <251946192@qq.com>

--- drivers/123/util.go
+++ drivers/123/util.go
@@ -163,9 +163,10 @@ func (d *Pan123) login() error {
 		SetHeaders(map[string]string{
 			"origin":      "https://www.123pan.com",
 			"referer":     "https://www.123pan.com/",
-			"user-agent":  "Dart/2.19(dart:io)-openlist",
-			"platform":    "web",
-			"app-version": "3",
+			"user-agent":  "123pan/v2.5.11(Android_7.1.2;Xiaomi)",
+			"platform":    "android",
+			"app-version": "61",
+			"x-app-version": "2.5.11"
 			//"user-agent":  base.UserAgent,
 		}).
 		SetBody(body).Post(SignIn)
@@ -202,9 +203,10 @@ do:
 		"origin":        "https://www.123pan.com",
 		"referer":       "https://www.123pan.com/",
 		"authorization": "Bearer " + d.AccessToken,
-		"user-agent":    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) openlist-client",
-		"platform":      "web",
-		"app-version":   "3",
+		"user-agent":    "123pan/v2.5.11(Android_7.1.2;Xiaomi)",
+		"platform":      "android",
+		"app-version":   "61",
+		"x-app-version": "2.5.11"
 		//"user-agent":    base.UserAgent,
 	})
 	if callback != nil {
